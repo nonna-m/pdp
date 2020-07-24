9983519e0266a1a4be8c0eaf52237605d05b0677
Nona1998 <musaielian.nona@mail.ru>
2020-07-24 20:13:23 +0400

    Improved Queue

--- main_queue.cpp
+++ main_queue.cpp
@@ -6,10 +6,10 @@ class Queue {
   unsigned  _size;
   int *_arr;
   public:
-  Queue() {
+  Queue(int size = 10) {
     _head = 0;
     _tail = 0;
-    _size = 10;
+    _size = size;
     _arr = new int [_size];
   }
   ~Queue() {
@@ -21,6 +21,7 @@ class Queue {
   unsigned int size();
   bool isEmpty();
   bool isFull();
+  void printQueue();
 };
 
 int Queue::dequeue() {
@@ -28,14 +29,25 @@ int Queue::dequeue() {
     std::cout << "Error: Queue is empty." << std::endl;
     return 1;
   }
-  return _arr[_head++];
+  _head += 1;
+  if (_head == _size) {
+    _head = 0;
+    return _arr[_size - 1];
+  }
+  return _arr[_head - 1];
 }
 
 void Queue::enqueue(int element) {
   if (isFull()) {
     std::cout << "Error: Queue is full." << std::endl;
   } else {
-    _arr[_tail++] = element;
+    _tail += 1;
+    if (_tail == _size) {
+      _tail = 0;
+      _arr[_size - 1] = element;
+    } else {
+      _arr[_tail - 1] = element;
+    }
   }
 }
 
@@ -59,24 +71,32 @@ bool Queue::isFull() {
   return size() == _size;
 }
 
+void Queue::printQueue() {
+  int el = _head;
+  std::cout << "Printing the queue: ";
+  for (int i = 0; i < size(); i++) std::cout << _arr[el++] << " ";
+  std::cout << std::endl;
+}
+
 int main() {
-  Queue queue;
-  std::cout << "queue.isEmpty() " << queue.isEmpty() << std::endl;
-  std::cout << "queue.peek() " << queue.peek() << std::endl;
-  std::cout << "queue.dequeue() " << queue.dequeue() << std::endl;
-  std::cout << "queue.size() " << queue.size() << std::endl;
+  Queue queue(10);
+  std::cout << "Is queue empty? " << queue.isEmpty() << std::endl;
+  std::cout << "First element in the queue: " << queue.peek() << std::endl;
+  std::cout << "Dequeue: " << queue.dequeue() << std::endl;
+  std::cout << "The size of the queue: " << queue.size() << std::endl;
   queue.enqueue(10);
-  std::cout << "enqueue 10 " << std::endl;
+  std::cout << "Enqueue 10." << std::endl;
   queue.enqueue(45);
-  std::cout << "enqueue 45" << std::endl;
-  std::cout << "queue.isEmpty()" << queue.isEmpty() << std::endl;
+  std::cout << "Enqueue 45." << std::endl;
   queue.enqueue(7);
-  std::cout << "enqueue 7" << std::endl;
-  std::cout << "queue.peek()" << queue.peek() << std::endl;
-  std::cout << "queue.dequeue() " << queue.dequeue() << std::endl;
+  std::cout << "Enqueue 7." << std::endl;
+  std::cout << "First element in the queue: " << queue.peek() << std::endl;
+  queue.printQueue();;
+  std::cout << "Dequeue: " << queue.dequeue() << std::endl;
   queue.enqueue(9);
-  std::cout << "enqueue 9" << std::endl;
-  std::cout << "queue.size() " << queue.size() << std::endl;
-  std::cout << "queue.isFull() " << queue.isFull() << std::endl;
+  std::cout << "Enqueue 9." << std::endl;
+  std::cout << "The size of the queue: " << queue.size() << std::endl;
+  std::cout << "Is queue full? " << queue.isFull() << std::endl;
+  queue.printQueue();
   return 0;
 }
