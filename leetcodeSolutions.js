abf63b4cffb41f696dd8a75ba1523ba63fdf98eb
nonna-m <37078836+nonna-m@users.noreply.github.com>
2021-06-29 08:53:50 +0400

    Course Schedule - Solved

--- leetcodeSolutions.js
+++ leetcodeSolutions.js
@@ -51,3 +51,35 @@ var searchMatrix = function (matrix, target) {
   }
   return false;
 };
+
+// 207. Course Schedule - https://leetcode.com/problems/course-schedule/
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+  // if (!prerequisites.length) return true;
+  // for (let i = 0; i < prerequisites.length; i++) {
+  //     if (prerequisites[i][0]===prerequisites[i][1]) return false;
+  // }
+  // const count = prerequisites.flat().reduce((acc, el) => acc.includes(el) ? acc : [...acc, el], []).length
+  // return prerequisites.length < count;
+  for (let i = 0; i < prerequisites.length; i++) {
+    let cycle = prerequisites[i][1];
+    const checkedIndexes = [];
+    // console.log(i, ' --- ');
+    for (let j = 0; j < prerequisites.length; j++) {
+      if (cycle == prerequisites[j][0] && !checkedIndexes.includes(j)) {
+        if (prerequisites[i][0] == prerequisites[j][1]) {
+          return false;
+        }
+        cycle = prerequisites[j][1];
+        checkedIndexes.push(j);
+        // console.log(j)
+        j = -1;
+      }
+    }
+  }
+  return true;
+};
